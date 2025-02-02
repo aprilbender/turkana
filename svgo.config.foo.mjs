4ef096e21f0675c1d7d9a0a2d080904e4d2c7153
April Johnson <bender.april@gmail.com>
2025-02-02 12:16:17 -0700

    Config file to control compression, possibly equivalent to the CLI version from svgo 1.x. Testing out...

--- svgo.config.foo.mjs
+++ svgo.config.foo.mjs
@@ -1,18 +1,28 @@
 export default {
+    // this is the equiv of the --pretty flag, I think
+    js2svg: { indent: 2, pretty: true },
     plugins: [
-      {
-        name: 'preset-default',
-        params: {
-          overrides: {
-            // disable a default plugin
-            cleanupIds: false,
+        // The default presets might include things that I'm not aware of,
+        // so for now I'm disabling them all for an opt-in approach.
+    //   {
+    //     name: 'preset-default',
+    //     params: {
+    //       overrides: {
+    //         // disable a default plugin
+    //         cleanupIds: false,
   
-            // customize the params of a default plugin
-            inlineStyles: {
-              onlyMatchedOnce: false,
-            },
-          },
-        },
+    //         // customize the params of a default plugin
+    //         inlineStyles: {
+    //           onlyMatchedOnce: false,
+    //         },
+    //       },
+    //     },
+    //   },
+      {
+        // This will prepend all IDs with the filename, munged with underscores. Example:
+        // Before: id="foo"
+        // After:  id="somefile_svg__foo"
+        name: "prefixIds",
       },
     ],
   };
